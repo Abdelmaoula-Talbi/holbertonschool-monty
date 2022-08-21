6af01a55fa7f2daa58326e4605092be277b754ce
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-08-21 21:49:57 +0100

    add some missing prototypes

--- monty.h
+++ monty.h
@@ -44,7 +44,9 @@ typedef struct instruction_s
 extern unsigned int line_number;
 void op_pall(stack_t **stack, __attribute__((unused)) unsigned int line_number);
 void op_pint(stack_t **stack, unsigned int line_number);
-
-
+void op_pop(stack_t **stack, unsigned int line_number);
+void op_swap(stack_t **stack, unsigned int line_number);
+void op_add(stack_t **stack, unsigned int line_number);
+void op_nop(__attribute__((unused)) stack_t **stack, __attribute__((unused)) unsigned int line_number);
 
 #endif
