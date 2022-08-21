0eb2e3256ff58bbc9b0d40f17ee63f8663c2bc5f
root <ali1998dehissy@gmail.com>
2022-08-21 12:32:35 -0700

    swap function

---
+++ op_swap.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+
+/**
+ * op_swap - swaps the top ewo elements of the stack
+ * @stack: list to swap its two elements
+ * @line_number: line number where the opcode swap appear
+ * Return: Nothing
+ */
+void op_swap(stack_t **stack, __attribute__((unused)) unsigned int line_number)
+{
+	stack_t *temp;
+
+	if ((*stack) == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	temp = (*stack);
+	(*stack)->next->prev = temp->prev;
+	(*stack)->next->next = temp;
+	(*stack)->next = temp->next->next;
+	(*stack)->prev = temp->next;
+	(*stack) = (*stack)->next;
+}
