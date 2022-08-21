924235c319a3022dadd97fa025fde16988bc32d6
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-08-21 21:42:00 +0100

    function that does nothing

---
+++ op_nop.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+
+/**
+ * op_nop -  doesn’t do anything.
+ * @stack: stack
+ * @line_number: line number where the nop opcode appears
+ * Return: Nothing
+ */
+void op_nop(__attribute__((unused)) stack_t **stack, __attribute__((unused)) unsigned int line_number)
+{
+}
